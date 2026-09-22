e64661b1362518947b1b73ae989a92438a78bd48
efeasafsd <155909988+efeasafsd@users.noreply.github.com>
2026-09-22 17:02:42 +0300

    Add Google verification route

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+﻿from flask import Flask, render_template, Response
 
 app = Flask(__name__)
 
@@ -9,6 +9,13 @@ def ana_sayfa():
 @app.route("/cv-olustur")
 def cv_olustur():
     return render_template("cv-olustur.html")
+
+@app.route("/google790c84f256f4f866.html")
+def google_verification():
+    return Response(
+        "google-site-verification: google790c84f256f4f866.html",
+        mimetype="text/plain"
+    )
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
